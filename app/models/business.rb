83f609a4d9eca3b1210c7e623e7540e77c46bf17
Cameron Barton <cameront21@gmail.com>
2014-01-17 21:54:12 -0800

    add validations for businesses

--- app/models/business.rb
+++ app/models/business.rb
@@ -6,4 +6,18 @@ class Business < ActiveRecord::Base
   def self.started
     where("years_in_business <= ?", Time.now).order("years_in_business asc")
   end
+
+  validates :title, :description, presence: true
+
+  validates :description, length: { maximum: 500 }
+
+  validates :website, format: {
+    with: /https?:\/\/[\S]+\b/i,
+    message: "must reference a valid URL"
+  }
+
+  validates :logo, format: {
+    with: /\w+\.(gif|jpg|png)\z/i,
+    message: "must reference a GIF, JPG, or PNG image"
+  }
 end
--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -8,4 +8,67 @@ describe "A business" do
 
     expect(Business.started).to eq([business1, business2, business3])
   end
+
+
+  it "requires a name" do
+    business = Business.new(title: "")
+
+    expect(business.valid?).to be_false
+    expect(business.errors[:title].any?).to be_true
+  end
+
+  it "requires a description" do
+    business = Business.new(description: "")
+
+    expect(business.valid?).to be_false
+    expect(business.errors[:description].any?).to be_true
+  end
+
+  it "accepts a description up to 500 characters" do
+    business = Business.new(description: "X" * 501)
+
+    expect(business.valid?).to be_false
+    expect(business.errors[:description].any?).to be_true
+  end
+
+  it "accepts a properly formatted website URLs" do
+    sites = %w[http://example.com https://example]
+    sites.each do |site|
+      business = Business.new(website: site)
+
+      expect(business.valid?).to be_false
+      expect(business.errors[:website].any?).to be_false
+    end
+  end
+
+  it "rejects improperly formatted website URLs" do
+    sites = %w[example.com http examplehttp]
+    sites.each do |site|
+      business = Business.new(website: site)
+
+      expect(business.valid?).to be_false
+      expect(business.errors[:website].any?).to be_true
+    end
+  end
+
+  it "accepts properly formatted image file names" do
+    file_names = %w[e.png event.png event.jpg event.gif EVENT.GIF]
+    file_names.each do |file_name|
+      business = Business.new(logo: file_name)
+
+      expect(business.valid?).to be_false
+      expect(business.errors[:logo].any?).to be_false
+    end
+  end
+
+  it "rejects improperly formatted image file names" do
+    file_names = %w[event .jpg .png .gif event.pdf event.doc]
+    file_names.each do |file_name|
+      business = Business.new(logo: file_name)
+
+      expect(business.valid?).to be_false
+      expect(business.errors[:logo].any?).to be_true
+    end
+  end
+
 end
